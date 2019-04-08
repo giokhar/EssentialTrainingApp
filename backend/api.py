a8461e45c02fa1aaaf1a5910869f20a36543589c
Giorgi Kharshiladze <giorgi.kharshila@gmail.com>
2019-04-08 01:10:09 -0400

    crud instaed of api.py

--- backend/api.py
+++ backend/api.py
@@ -6,7 +6,7 @@ from rest_framework import viewsets, permissions
 from rest_framework.decorators import action
 from rest_framework.response import Response
 # helpers directory
-from backend.helpers.api import get_quiz_details_json
+from backend.helpers.crud import get_quiz_details_json
 
 class StudentViewSet(viewsets.ModelViewSet):
 	queryset = Student.objects.all()
